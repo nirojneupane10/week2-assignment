102e8957dc5da0c373fc9045b97aedb63fdda25e
nirojneupane10 <niroj.neupane@varicon.com.au>
2024-08-14 13:37:53 +0545

    Calculation of highest sales product

---
+++ highestSales.js
@@ -0,0 +1,19 @@
+//Calculation of the product with highest sales
+import { salesData } from "./salesData.js";
+
+const totalSales = salesData.reduce((acc, { product, price, quantity }) => {
+  const total = price * quantity;
+  acc[product] = (acc[product] ?? 0) + total;
+  return acc;
+}, {});
+
+const [topSellingProduct, maxSales] = Object.entries(totalSales).reduce(
+  (topProduct, currentProduct) => {
+    return currentProduct[1] > topProduct[1] ? currentProduct : topProduct;
+  },
+  ["", 0]
+);
+
+console.log(
+  `Top Selling Product: ${topSellingProduct} with Total Sales: Rs ${maxSales}`
+);
