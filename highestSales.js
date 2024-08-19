49cbf57037756c4f6e29b75f9ca0fb6fc7190eda
nirojneupane10 <niroj.neupane@varicon.com.au>
2024-08-19 13:22:27 +0545

    Reduce time complexity

--- highestSales.js
+++ highestSales.js
@@ -1,17 +1,24 @@
 //Calculation of the product with highest sales
 import salesData from "./salesData.json" assert { type: "json" };
 
-const totalSales = salesData.reduce((acc, { product, price, quantity }) => {
-  const total = price * quantity;
-  acc[product] = (acc[product] ?? 0) + total;
-  return acc;
-}, {});
+const { topSellingProduct, maxSales } = salesData.reduce(
+  (acc, { product, price, quantity }) => {
+    const total = price * quantity;
 
-const [topSellingProduct, maxSales] = Object.entries(totalSales).reduce(
-  (topProduct, currentProduct) => {
-    return currentProduct[1] > topProduct[1] ? currentProduct : topProduct;
+    acc.totalSales[product] = (acc.totalSales[product] ?? 0) + total;
+
+    if (acc.totalSales[product] > acc.maxSales) {
+      acc.maxSales = acc.totalSales[product];
+      acc.topSellingProduct = product;
+    }
+
+    return acc;
   },
-  ["", 0]
+  {
+    totalSales: {},
+    topSellingProduct: "",
+    maxSales: 0,
+  }
 );
 
 console.log(
